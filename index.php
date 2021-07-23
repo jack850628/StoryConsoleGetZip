fba078a3e3a1550b24068ba2902bb72401f10cad
jack850628 <jack850628@hotmail.com>
2021-07-23 11:42:00 +0800

    改為檔案下載後不解壓縮，直接回傳

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-define('STORY_DIR', 'story');
+header('Access-Control-Allow-Origin: https://jack850628.github.io');
 
 //$url = 'https://gitlab.com/jack850628/storyconsolestorycollect/-/raw/main/test.zip';
 if(!isset($_GET['url'])){
@@ -10,20 +7,7 @@ if(!isset($_GET['url'])){
 	exit(0);
 }
 $url = $_GET['url'];
-
-$uuid = uniqid();
-mkdir($uuid);
-chdir($uuid);
-`wget $url`;
-$fileName = scandir('.')[2];
-system("unzip $fileName -d ".STORY_DIR);
-$files = scandir('./'.STORY_DIR);
-$storyObj = [];
-for($i=2;$i<count($files);$i++){
-	$storyObj[explode('.', $files[$i])[0]] = json_decode(file_get_contents('./'.STORY_DIR.'/'.$files[$i]));
-}
-//var_dump($storyObj);
-chdir('..');
-`rm -rf $uuid`;
-ob_clean();
-echo json_encode($storyObj);
+header('Content-Type: application/octet-stream');
+header('Content-Transfer-Encoding: Binary'); 
+header('Content-disposition: attachment; filename="story.zip"'); 
+readfile($url);
