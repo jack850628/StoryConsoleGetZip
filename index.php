50c09a381d495c876154a500bebc19af0a131863
jack850628 <jack850628@hotmail.com>
2021-07-23 23:10:15 +0800

    修正部分連結下載時 Content-Length 會為 0 的問題

--- index.php
+++ index.php
@@ -3,11 +3,22 @@ header('Access-Control-Allow-Origin: https://jack850628.github.io');
 
 //$url = 'https://gitlab.com/jack850628/storyconsolestorycollect/-/raw/main/test.zip';
 if(!isset($_GET['url'])){
-	echo json_encode([]);
 	exit(0);
 }
 $url = $_GET['url'];
 header('Content-Type: application/zip');
 header('Content-Transfer-Encoding: Binary'); 
-header('Content-disposition: attachment; filename="story.zip"'); 
-readfile($url);
+header('Content-disposition: attachment; filename="story.zip"');
+
+$uuid = uniqid();
+mkdir($uuid);
+chdir($uuid);
+`wget $url`;
+
+$fileName = scandir('.')[2];
+ob_clean();
+$fileSize = readfile('./' . $fileName);
+header('Content-Length: ' . $fileSize);
+
+chdir('..');
+`rm -rf $uuid`;
